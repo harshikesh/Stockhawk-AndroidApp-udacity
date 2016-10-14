eeab598a7494465b8a02102433056858ac3e084f
Harshikesh Kumar <harshikeshkumar@gmail.com>
2016-10-14 23:02:16 +0530

    removing hardcoded strings

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -126,7 +126,7 @@ public class MyStocksActivity extends AppCompatActivity
                           new String[] { input.toString() }, null);
                       if (c.getCount() != 0) {
                         Toast toast =
-                            Toast.makeText(MyStocksActivity.this, "This stock is already saved!",
+                            Toast.makeText(MyStocksActivity.this, R.string.already_in_stock,
                                 Toast.LENGTH_LONG);
                         toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
                         toast.show();
